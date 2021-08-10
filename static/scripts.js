38043920aee6d0b9d33a2ceeb0d8f9454ad21702
leodedsec <azat.abdrashitov7@gmail.com>
2021-08-10 16:41:36 +0300

    upd scrips

--- static/scripts.js
+++ static/scripts.js
@@ -1,53 +1,117 @@
+let selected_widgets = new Array();
+let status_style_obj = new Object();
+
+
+//Открыть виджет
 function Open_Widget(obj) {
   let name = obj;
-  window.open(`http://localhost:8080/open_widget?name_widget=${name}`,'_blank');
-  };
+  if (typeof(obj)=="string"){
+    window.open(`http://localhost:8080/open_widget?name_widget=${name}`,'_blank');
+  } else {
+    for (let value of obj){
+      window.open(`http://localhost:8080/open_widget?name_widget=${value}`,'_blank');
+    }
+  }
+};
 
 
+//Показать-Скрыть виджет
 function Show_Hide_Widget(obj) {
-    let name = obj;
+  if (typeof(obj)=="string"){
+    let name = new TextEncoder().encode(obj);
     let xhr = new XMLHttpRequest();
     xhr.open("GET", "http://localhost:8080/edit_status/");
     xhr.setRequestHeader("name_widget",name);
     xhr.send();
-  };
-
-
-function Delete(obj) {
-  let name = obj;
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "http://localhost:8080/delete_widget/");
-  xhr.setRequestHeader("name_widget",name);
-  xhr.responseType = 'json';
-  xhr.send();
-  xhr.onload = function () {
-    let status = xhr.response;
-    if (status==true && name!=""){
-      alert("Виджет успешно удален!")
-    } else {
-      alert("Возникла ошибка, виджет не удален!")
+  } else {
+    for (let value of selected_widgets){
+      let name = new TextEncoder().encode(value);
+      let xhr = new XMLHttpRequest();
+      xhr.open("GET", "http://localhost:8080/edit_status/");
+      xhr.setRequestHeader("name_widget",name);
+      xhr.send()
     }
-
   }
-  parent.location.reload();
 };
 
 
+//Удалить виджет
+function Delete(obj) {
+  if (typeof(obj)=="string"){
+    let name = new TextEncoder().encode(obj);
+    let fix_name = new TextDecoder("UTF-8").decode(name);
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:8080/delete_widget/");
+    xhr.setRequestHeader("name_widget",name);
+    xhr.responseType = 'json';
+    xhr.send();
+    xhr.onload = function () {
+      let status = xhr.response;
+      if (status==true && name!=""){
+        alert(`Виджет "${fix_name}" успешно удален!`)
+        parent.location.reload();
+      } else {
+        alert(`Возникла ошибка, виджет "${fix_name}" не удален!`)
+        parent.location.reload();
+      }
+    }
+  } else {
+    for (let value of selected_widgets){
+      let name = new TextEncoder().encode(value);
+      let xhr = new XMLHttpRequest();
+      xhr.open("POST", "http://localhost:8080/delete_widget/");
+      xhr.setRequestHeader("name_widget",name);
+      xhr.responseType = 'json';
+      xhr.send();
+      xhr.onload = function () {
+        parent.location.reload();
+        }
+      }
+    }
+};
+
+
+//Показать инфу о виджете
 function Show_Info(obj) {
-  let name = obj;
+  let name = new TextEncoder().encode(obj);
+  let fix_name = new TextDecoder("UTF-8").decode(name);
   let xhr = new XMLHttpRequest();
   xhr.open("GET", "http://localhost:8080/about_widget/");
   xhr.setRequestHeader("name_widget",name);
   xhr.responseType = 'json';
   xhr.send();
   xhr.onload = function () {
-    alert(name+"\n"+xhr.response.title+"\n"+xhr.response.subtitle);
+    alert(fix_name+"\n"+xhr.response.title+"\n"+xhr.response.subtitle);
   }
 };
 
 
-let status_style_obj = new Object();
+//Проверить чекбокс и добавить инфу в div
+function CheckBox(obj){
+  let element = document.getElementById('selected_widgets');
 
+  if (selected_widgets.includes(obj)==false){
+    selected_widgets.push(obj)
+    element.textContent = selected_widgets;
+  } else {
+    let index = selected_widgets.indexOf(obj);
+    if (index > -1) {
+      selected_widgets.splice(index,1);
+      element.textContent = selected_widgets;
+  }
+}
+};
+
+
+//Открыть список виджетов в отдельных вкладках
+function GetSelectedWidgets() {
+  for (let value of selected_widgets){
+    window.open(`http://localhost:8080/open_widget?name_widget=${value}`,'_blank');
+    }
+};
+
+
+//Показать всплывающую форму
 function ShowFormEdit(obj) {
   if (obj in status_style_obj){
     if (status_style_obj[obj]=="none"){
@@ -64,6 +128,7 @@ function ShowFormEdit(obj) {
 }
 
 
+//Скрыть всплывающую форму
 function HideFormEdit(obj) {
     document.getElementById(obj).style.display="none";
 }
